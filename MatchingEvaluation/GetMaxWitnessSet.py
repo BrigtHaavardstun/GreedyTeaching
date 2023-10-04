2ca6e0b41f734c1b688dc120642d776acea38e99
Brigt Arve Toppe Håvardstun <brigt.t.havardstun@uib.no>
2023-10-04 23:48:34 +0200

    [MatchMaxWitSet] Swapp to use utils for weights

--- MatchingEvaluation/GetMaxWitnessSet.py
+++ MatchingEvaluation/GetMaxWitnessSet.py
@@ -1,17 +1,9 @@
-import json
 from utils.load_matching import load_witness
-
-
-def get_witness_weights(folder):
-    witness_sets = json.loads(
-        open(f"{folder}/graph-witness-sets.json", "r").read())
-    witness_weights = {key: int(value[1])
-                       for key, value in witness_sets.items()}
-    return witness_weights
+from utils.load_graph import load_witness_weights
 
 
 def witness_set_size(folder, witness_id):
-    return get_witness_weights(folder)[witness_id]
+    return load_witness_weights(folder)[witness_id]
 
 
 def get_max_witness_set_size(folder, matching):
